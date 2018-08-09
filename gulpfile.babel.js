3cd2fef1b60ba195530c6e0408481eaeab6c3bde
Tenderfeel <tenderfeel@gmail.com>
2018-08-10 02:18:36 +0900

    imagemin

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import webpackConfig from './webpack.config.js';
 import webpack from 'webpack';
 import sass from 'gulp-sass';
 import pug from 'gulp-pug';
+import imagemin from 'gulp-imagemin';
 
 const paths = {
   scripts: {
@@ -19,6 +20,10 @@ const paths = {
     src: ['src/**/*.pug', '!src/**/_*.pug'],
     watch: ['src/**/*.pug', 'src/**/_*.pug'],
     dest: 'dist/'
+  },
+  images: {
+    src: 'src/assets/images/**/*',
+    dest: 'dist/assets/images/'
   }
 };
 
@@ -47,15 +52,26 @@ export function htmls () {
   .pipe(gulp.dest(paths.htmls.dest));
 }
 
+//画像
+export function images() {
+  return gulp.src(paths.images.src)
+       .pipe(imagemin({
+         verbose: true
+       }))
+       .pipe(gulp.dest(paths.images.dest))
+}
+
 //Watch
 export function watch() {
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.htmls.watch,  htmls);
+  gulp.watch(paths.images.src, images);
 }
 
-const build = gulp.series(clean, gulp.parallel(htmls, scripts, styles));
-const _default = gulp.series(clean, gulp.parallel(htmls, scripts, styles, watch));
+
+const build = gulp.series(clean, gulp.parallel(htmls, images, scripts, styles));
+const _default = gulp.series(clean, gulp.parallel(htmls, images, scripts, styles, watch));
 
 gulp.task('build', build);
 

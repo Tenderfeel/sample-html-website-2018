d9093289f0497feb765335a79ca4268ae2062f9b
Tenderfeel <tenderfeel@gmail.com>
2018-08-10 01:23:26 +0900

    sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,12 +3,17 @@ import del from 'del';
 import webpackStream from 'webpack-stream';
 import webpackConfig from './webpack.config.js';
 import webpack from 'webpack';
+import sass from 'gulp-sass';
 
 const paths = {
   scripts: {
     src: 'src/assets/js/**/*.js',
     dest: 'dist/assets/js/'
-  }
+  },
+  styles: {
+    src: 'src/assets/scss/**/*.scss',
+    dest: 'dist/assets/css/'
+  },
 };
 
 //clean up
@@ -20,13 +25,23 @@ export function scripts () {
   .pipe(gulp.dest(paths.scripts.dest));
 }
 
+//CSS
+export function styles() {
+return gulp.src(paths.styles.src)
+      .pipe(sass({
+        outputStyle: 'nested'
+      }).on('error', sass.logError))
+      .pipe(gulp.dest(paths.styles.dest));
+}
+
 //Watch
 export function watch() {
   gulp.watch(paths.scripts.src, scripts);
+  gulp.watch(paths.styles.src, styles);
 }
 
-const build = gulp.series(clean, gulp.parallel(scripts));
-const _default = gulp.series(clean, gulp.parallel(scripts, watch));
+const build = gulp.series(clean, gulp.parallel(scripts, styles));
+const _default = gulp.series(clean, gulp.parallel(scripts, styles, watch));
 
 gulp.task('build', build);
 
